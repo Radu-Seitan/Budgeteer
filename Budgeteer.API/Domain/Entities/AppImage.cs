26c7840ccd2ebcc8b47dce74498c50b7a8c35a8d
Catalina Gheorghioiu <catalina.gheorghioiu@amdaris.com>
2024-03-11 20:41:44 +0200

    BUD-11 added intial domain models and migration

---
+++ Budgeteer.API/Domain/Entities/AppImage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budgeteer.Domain.Entities
+{
+    public class AppImage
+    {
+        public Guid Id { get; set; }
+        public byte[] Content { get; set; }
+        public string Type { get; set; }
+        public int StoreId { get; set; }
+        public List<Store> Stores { get; set; }
+    }
+}
---
+++ Budgeteer.API/Domain/Entities/Store.cs
@@ -0,0 +1,10 @@
+﻿namespace Budgeteer.Domain.Entities
+{
+    public class Store : BaseEntity
+    {
+        public string Name { get; set; }
+        public Guid? ImageId { get; set; }
+        public AppImage Image { get; set; }
+        public List<Expense> Expenses { get; set; }
+    }
+}
