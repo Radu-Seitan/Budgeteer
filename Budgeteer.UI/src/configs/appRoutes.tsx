bcb2c47b10e2e6a2a3fcb28d2030df60cfd50cf3
biancabelea <80485735+biancabelea@users.noreply.github.com>
2024-05-22 00:49:47 +0300

    Register page

--- Budgeteer.UI/src/configs/appRoutes.tsx
+++ Budgeteer.UI/src/configs/appRoutes.tsx
@@ -4,11 +4,13 @@ import { AuthRoute } from '../auth/AuthRoute';
 import App from '../App';
 import { Login } from '../components/Login';
 import { Home } from '../components/Home';
+import { Register } from '../components/Register';
 
 export const AppRoutes: FC = () => {
     return (
         <Routes>
             <Route path={'/'} element={<App />}>
+                <Route path={'/register'} element={<Register />} />
                 <Route path={'/login'} element={<Login />} />
                 <Route
                     path={'/'}
